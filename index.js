226178a4dc49133e332bb998a3684a64489dd9bf
Jonathan Scott <jonathan.scott@gmail.com>
2020-03-04 13:22:36 -0800

    db setup and initial function

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp()
+const db = admin.firestore()
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +9,9 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.users = functions.https.onRequest(async (request, response) => {
+    const snapshot = await db.collection('users').get()
+    const users = snapshot.empty ? [] : snapshot.docs.map(doc => Object.assign(doc.data(), {id: doc.id}))
+    response.send(users)
+})
